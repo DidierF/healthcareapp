523757bb80ca193afbc3bdd308cffa0e700ad92d
didier <didier.fuentes@gmail.com>
2016-02-17 01:11:49 -0400

    Updated doctors model in migration.

--- django_project/hca_app/models.py
+++ django_project/hca_app/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 
+USER_TYPES = (
+    ('admin', 'Administrator'),
+    ('std', 'Standard'),
+    ('lmtd', 'Limited')
+)
 
+
+# Users table
 class Doctor(models.Model):
     doctorId = models.IntegerField(primary_key=True)
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=50, unique=True)
+    document = models.CharField(max_length=20, unique=True)
+    password = models.CharField(max_length=50)
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    cellphone = models.CharField(max_length=10)
+    officePhone = models.CharField(max_length=10)
+    userType = models.CharField(max_length=5, choices=USER_TYPES, default='std')
