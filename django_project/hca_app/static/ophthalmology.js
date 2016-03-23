4286b7a4caa5c6be357a0d7865d545ede5870ee9
didier <didier.fuentes@gmail.com>
2016-03-22 23:42:22 -0400

    Deletes ophthalmology forms.

--- django_project/hca_app/static/ophthalmology.js
+++ django_project/hca_app/static/ophthalmology.js
@@ -10,3 +10,25 @@ $('#button-id-savebtn').on('click', function(){
         console.log(jqXHR);
     });
 });
+
+$('#deleteBtn').on('click', function(){
+    $.ajax({
+        url: '/api/v1/ophthalmology/' + $('#form_id').val(),
+        method: 'delete',
+        beforeSend: function(jqXHR){
+            jqXHR.setRequestHeader('X-CSRFToken',$('input[name=csrfmiddlewaretoken]').val());
+        }
+    })
+    .done(function(data, textStatus, jqXHR) {
+        switch(jqXHR.status){
+            case 204:
+                console.log('Ophth form deleted');
+//                window.location.href = '/'
+                history.go(-1);
+                break;
+            case 404:
+                console.log('Ophth form  was not found.');
+                break;
+        }
+    });
+});
